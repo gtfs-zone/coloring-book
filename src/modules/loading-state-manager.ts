f64a4056ab67a72a537a6afe37836a762fcdfb41
Max Katz-Christy <maxkatzchristy@gmail.com>
2026-03-31 21:20:24 +0200

    refactor: knip

--- src/modules/loading-state-manager.ts
+++
@@ -1,5 +0,0 @@
-// Renamed to feed-progress-indicator.ts
-export {
-  FeedProgressIndicator as LoadingStateManager,
-  feedProgressIndicator as loadingStateManager,
-} from './feed-progress-indicator.js';
